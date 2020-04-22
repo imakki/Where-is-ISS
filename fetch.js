cba6dcff7bd2cc71e305b278368ebdd42d5d6f85
Ankit <ankityc143>
2020-04-22 16:06:12 +0530

    added lat and lon and marker on map

--- fetch.js
+++ fetch.js
@@ -1,6 +1,19 @@
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+//making map and tiles
+const mymap = L.map('issMap').setView([0,0],1);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetmap</a>';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl,{attribution});
+tiles.addTo(mymap);
 
-const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+//making custom icon
+const myIcon = L.icon({
+    iconUrl: 'iss200.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+});
+
+const marker = L.marker([0,0],{icon: myIcon}).addTo(mymap);
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 
 async function getISS() {
@@ -9,6 +22,8 @@ async function getISS() {
     const data = await response.json();
     
     const {latitude,longitude} = data;
+
+    marker.setLatLng([latitude,longitude]);
     
     document.getElementById('lat').textContent = latitude;
     document.getElementById('lon').textContent = longitude;
